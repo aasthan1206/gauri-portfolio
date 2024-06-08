4313c8e68ac3554e2a0093177d8686b89ddc1520
aasthan1206 <aastha.nayak2002@gmail.com>
2024-06-08 10:55:11 +0530

    removed art description

--- src/pages/Art.jsx
+++ src/pages/Art.jsx
@@ -16,17 +16,17 @@ const Art = () => {
 
   return (
     <div className="m-8 lg:flex justify-evenly">
-      <div className="flex justify-center">
+      <div className="flex justify-center self-center">
         <img
           src={artItem.img}
           alt=""
           className="w-[300px] h-[300px] lg:w-[475px] lg:h-[475px] lazyload"
         />
       </div>
-      <div className="text-center py-8 lg:text-left">
+      {/* <div className="text-center py-8 lg:text-left">
         <div>Title</div>
         <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-      </div>
+      </div> */}
     </div>
   );
 };
